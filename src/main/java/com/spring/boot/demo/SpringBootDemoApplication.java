dcc19d0d832b95599b6c7a3739ddb9c21df00c2e
吕晶048807 <lvjing@renmaitech.com>
2020-12-16 19:09:47 +0800

    chore:工程调整

--- src/main/java/com/spring/boot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/spring/boot/demo/SpringBootDemoApplication.java
@@ -2,12 +2,14 @@ package com.spring.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author Jinglv
  * @date 2020/10/10
  */
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
