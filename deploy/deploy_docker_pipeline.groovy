17155b7021ba407846dd9cfb00b555b1f4b5173e
吕晶048807 <lvjing@renmaitech.com>
2021-03-02 15:12:45 +0800

    chore:部署Jenkinsfile内容

--- deploy/deploy_docker_pipeline.groovy
+++ deploy/deploy_docker_pipeline.groovy
@@ -27,7 +27,6 @@ pipeline {
                     . ~/.bash_profile 
                     
                     cd ${WORKSPACE}
-                    // 注入jacoco插件配置,clean test执行单元测试代码. All tests should pass.
                     mvn org.jacoco:jacoco-maven-plugin:prepare-agent -f ${pomPath} clean install -Dautoconfig.skip=true -Dmaven.test.skip=false
                 '''
             }
@@ -41,7 +40,6 @@ pipeline {
                     cd ${WORKSPACE}
                     mvn sonar:sonar -Dsonar.projectKey=spring-boot-restful-api -Dsonar.host.url=http://60.205.228.49:9000/ -Dsonar.login=053ed1077e82a2bb36eebf619d24d75b8c5738b9 -Dsonar.branch.name=${branch}
                     junit '**/target/surefire-reports/*.xml'
-                    // 配置单元测试覆盖率要求，未达到要求pipeline将会fail,code coverage.LineCoverage>20%.
                     jacoco changeBuildStatus: true, maximumLineCoverage:"20"
                 '''
             }
